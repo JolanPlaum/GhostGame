b55c29b36432d6675671dd795092c26cafddb1f6
Jolan Plaum <jolan.plaum@student.howest.be>
2023-10-16 00:00:34 +0200

    Improve jump behavior
    Prevent being able to double jump by calling jump more than once before a fixed update has happened.

--- Assets/Scripts/Behaviors/JumpBehavior.cs
+++ Assets/Scripts/Behaviors/JumpBehavior.cs
@@ -6,6 +6,7 @@ public class JumpBehavior : MonoBehaviour
 {
 	[SerializeField] protected float _jumpForce = 10.0f;
 	protected Rigidbody _rigidbody;
+	protected bool _jumpedLastFrame;
 
 	private bool _isOnGround = false;
 	public bool IsOnGround
@@ -22,15 +23,26 @@ public class JumpBehavior : MonoBehaviour
 	// Check if the rigidbody is on the ground
 	private void FixedUpdate()
 	{
+		if (_jumpedLastFrame)
+		{
+			_isOnGround = false;
+			_jumpedLastFrame = false;
+			return;
+		}
+
 		float offset = 0.1f;
 
-		_isOnGround = Physics.Raycast(transform.position + Vector3.up * offset, Vector3.down, offset * 2);
+		_isOnGround = Physics.Raycast(transform.position + Vector3.up * offset, Vector3.down, offset * 2) && _rigidbody.velocity.y <= 0;
 	}
 
 	// Jump function
 	public virtual void Jump()
 	{
 		if (_isOnGround)
+		{
 			_rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+			_jumpedLastFrame = true;
+			_isOnGround = false;
+		}
 	}
 }
