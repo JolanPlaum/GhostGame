262c3324774ec7f68b1bdc5bb4a2ae79324beb61
Jolan Plaum <jolan.plaum@student.howest.be>
2023-10-16 03:15:56 +0200

    Update dash behavior

--- Assets/Scripts/Behaviors/DashBehavior.cs
+++ Assets/Scripts/Behaviors/DashBehavior.cs
@@ -6,19 +6,21 @@ public class DashBehavior : MonoBehaviour
 {
     [SerializeField] private float _dashSpeed = 50f;
     [SerializeField] private float _dashDistance = 5f;
+	[SerializeField] private TrailRenderer _trail;
 	private Rigidbody _rigidbody;
 	private float _distanceSoFar;
 
 	private bool _isDashing = false;
-    public bool IsDashing
-    {
-        get { return _isDashing; }
+	public bool IsDashing
+	{
+		get { return _isDashing; }
 		private set
 		{
 			_isDashing = value;
 
 			if (_isDashing) _distanceSoFar = 0;
 			if (_rigidbody) _rigidbody.useGravity = !_isDashing;
+			if (_trail) _trail.enabled = _isDashing;
 		}
 	}
 
@@ -26,6 +28,7 @@ public class DashBehavior : MonoBehaviour
 	protected virtual void Awake()
 	{
 		_rigidbody = GetComponent<Rigidbody>();
+		if (_trail) _trail.enabled = false;
 	}
 
 	// Dashing is movement so do it in fixed update
@@ -44,6 +47,13 @@ public class DashBehavior : MonoBehaviour
 				travelDistance -= _distanceSoFar - _dashDistance;
 			}
 
+			// Check if a wall will be hit
+			if (Physics.Raycast(transform.position + transform.up, transform.forward, out RaycastHit hit, travelDistance))
+			{
+				IsDashing = false;
+				travelDistance = hit.distance * 0.9f;
+			}
+
 			// Move this object in the direction it's facing
 			transform.position += transform.forward * travelDistance;
 		}
@@ -54,7 +64,8 @@ public class DashBehavior : MonoBehaviour
     {
 		if (_rigidbody == null || IsDashing) return false;
 
-		if (_rigidbody.velocity.y == 0)
+		// Not sure why but sometimes unity leaves the velocity at -2.384186E-06
+		if (Mathf.Abs(_rigidbody.velocity.y) <= 0.00001)
 		{
 			IsDashing = true;
 			return true;
