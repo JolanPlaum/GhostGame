a1825bd983dcc220e00b0b570f9d0aa300ecd297
Jolan Plaum <jolan.plaum@student.howest.be>
2023-10-22 23:36:33 +0200

    Add level reset to game mode

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameMode : MonoBehaviour
 {
@@ -21,7 +22,12 @@ public class GameMode : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+        // Close the game
 		if (Input.GetKeyDown(KeyCode.Escape))
 			Application.Quit();
+
+		// Reset the level
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 }
