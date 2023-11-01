be3bec94256d36178f9f6466e621178d85b4484a
Jolan Plaum <jolan.plaum@student.howest.be>
2023-11-01 22:46:16 +0100

    Create DoorBehavior & KeyID script

---
+++ Assets/Scripts/Behaviors/Interactable/DoorBehavior.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehavior : InteractableBehavior
+{
+	[SerializeField] private GameObject _lockedVisuals;
+	[SerializeField] private GameObject _openedVisuals;
+	private bool _isOpen = false;
+
+	// Ensure the door visuals are set to locked
+	private void Awake()
+	{
+		if (_lockedVisuals) _lockedVisuals.SetActive(true);
+		if (_openedVisuals) _openedVisuals.SetActive(false);
+	}
+
+	// Open door behavior
+	public override void Execute(GameObject other)
+	{
+		// Check if the door is already open
+		if (_isOpen) return;
+
+		// Check if other object has a matching key
+		GrabBehavior behavior = other.GetComponent<GrabBehavior>();
+		if (behavior == null) return;
+		if (behavior.HeldObject == null) return;
+		if (behavior.HeldObject.GetComponent<KeyID>() == null) return;
+		Destroy(behavior.HeldObject);
+
+		// Switch the visuals to open
+		if (_lockedVisuals) _lockedVisuals.SetActive(false);
+		if (_openedVisuals) _openedVisuals.SetActive(true);
+
+		// Disable the door collision
+		Collider collider = GetComponent<Collider>();
+		if (collider != null) collider.enabled = false;
+
+		// Set the door as open
+		_isOpen = true;
+	}
+}
